5f90a8c85af9557f2d29945d8c5b3d1c98f4f88e
Kamil Bednarz <kamil.bednarz@u2i.com>
2022-02-10 14:08:44 +0100

    Update polish words database

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
-require_relative 'literalnie/database'
 require_relative 'literalnie/heuristic_builder'
 require_relative 'literalnie/lookup'
 
-# average = words.map { |word| Literalnie::Lookup.new(words, word).solve.length }.sum / words.length.to_f
-# puts average
-
-words = Literalnie::Database::POLISH
+words = File.open('./words/pl_pl.txt').read.split("\n")
 word_to_guess = words.sample
+
 Literalnie::Lookup.new(words, word_to_guess).solve(verbose: true)
+
+# sum = 0
+# words.sample(100).each_with_index do |word, index|
+#   puts "processed #{index} rows" if index % 100 == 0
+#   sum += Literalnie::Lookup.new(words, word).solve.length
+# end
+# puts sum / words.length.to_f
